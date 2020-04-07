e3fab993636ff58e76c2b40f7184f93c6bb03bc1
MateuszBach <mateusz.bachurski@gmail.com>
2020-04-07 18:58:54 +0200

    Add Sliderhead sliderText and spanwrite

--- script.js
+++ script.js
@@ -1,6 +1,68 @@
 const navBtns = document.body.querySelectorAll(".tableNav a");
 const headerImg = document.body.querySelector(".imgHeader ")
 /****Rwd****/
+/*****Slider header *****/
+
+
+const slideListHeader = [{
+        img: "img/imgNavDesktop1b.jpg",
+        textH1: "Praca Zespołowa",
+    },
+    {
+        img: "img/imgNavDesktop2b.jpg",
+        textH1: "Myślenie Kreatywne",
+    },
+    {
+        img: "img/imgNavDesktop3b.jpg",
+        textH1: "Burza Mózgów",
+    }
+];
+
+const arrayTxtSpan = ["Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa corporis aut accusantium nihil illum fugiat dolorum nam id modi sit!", "Porem ipsum dolor sit amet consectetur adipisicing elit.Ipsa corporis aut accusantium nihil illum fugiat dolorum nam id modi sit!", "Zorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa corporis aut accusantium nihil illum fugiat dolorum nam id modi sit!"]
+const image = document.querySelector('img.slider');
+const h1 = document.querySelector('h1.slider');
+const spnText = document.querySelector('span.slider');
+let active = 0;
+let activeLetter = 0;
+let activeText = 0;
+const timeSlide = 5000;
+let indexTxt = 0;
+const timeLetter = 20;
+const addLetter = () => {
+    spnText.textContent += arrayTxtSpan[activeText][activeLetter];
+    activeLetter++;
+    if (activeLetter === arrayTxtSpan[activeText].length) {
+        if (changeSlide) {
+            activeText++
+        };
+        if (activeText === arrayTxtSpan.length) {
+            activeText = 0;
+        }
+        return setTimeout(() => {
+            activeLetter = 0;
+            spnText.textContent = '';
+            addLetter();
+        }, timeSlide - (arrayTxtSpan[activeText].length * timeLetter) - 74);
+
+    }
+    setTimeout(addLetter, timeLetter)
+}
+setTimeout(addLetter(), timeSlide);
+const changeSlide = () => {
+    active++;
+    if (active === slideListHeader.length) {
+        active = 0;
+    }
+    image.src = slideListHeader[active].img;
+    h1.textContent = slideListHeader[active].textH1;
+    spnText.textContent = ''
+}
+setInterval(changeSlide, timeSlide);
+
+
+
+
+
 navBtns.forEach(navBtn => {
     const noRefresh = (e) => {
         e.preventDefault();
@@ -19,15 +81,13 @@ for (let i = 0; i < projektDivy.length; i++) {
     }
 }
 /*** Text on Img Header *******/
-const spnText = document.querySelector('.textSpan');
-const txt = "Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa corporis aut accusantium nihil illum fugiat dolorum nam id modi sit!";
-let indexTxt = 0;
-const time = 20;
 
-const addLetter = () => {
-    spnText.textContent += txt[indexTxt];
-    indexTxt++;
-    if (indexTxt === txt.length)
-        clearInterval(indexTyping);
-}
-const indexTyping = setInterval(addLetter, time);
+
+
+
+
+/******Projekty showdiv *****/
+
+// window.addEventListener("scroll", function () {
+//     if (projektyDivy.offsetHeight = window.innerHeight) {}
+// })
