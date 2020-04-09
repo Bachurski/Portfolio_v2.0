f2bf14ad3a1ba467da38b91e1bfe8ec4d4430362
MateuszBach <mateusz.bachurski@gmail.com>
2020-04-10 01:14:15 +0200

    Reapair slider and addLetter

--- script.js
+++ script.js
@@ -7,18 +7,20 @@ const headerImg = document.body.querySelector(".imgHeader ")
 const slideListHeader = [{
         img: "img/imgNavDesktop1b.jpg",
         textH1: "Praca Zespołowa",
+        arrayTxtSpan: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa corporis aut accusantium nihil illum fugiat dolorum nam id modi sit!"
     },
     {
         img: "img/imgNavDesktop2b.jpg",
         textH1: "Myślenie Kreatywne",
+        arrayTxtSpan: "Porem ipsum dolor sit amet consectetur adipisicing elit.Ipsa corporis aut accusantium nihil illum fugiat dolorum nam id modi sit!",
     },
     {
         img: "img/imgNavDesktop3b.jpg",
         textH1: "Burza Mózgów",
+        arrayTxtSpan: "Zorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa corporis aut accusantium nihil illum fugiat dolorum nam id modi sit!",
     }
 ];
-
-const arrayTxtSpan = ["Lorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa corporis aut accusantium nihil illum fugiat dolorum nam id modi sit!", "Porem ipsum dolor sit amet consectetur adipisicing elit.Ipsa corporis aut accusantium nihil illum fugiat dolorum nam id modi sit!", "Zorem ipsum dolor sit amet consectetur adipisicing elit.Ipsa corporis aut accusantium nihil illum fugiat dolorum nam id modi sit!"]
+/**** var to Header slider add letter ******/
 const image = document.querySelector('img.slider');
 const h1 = document.querySelector('h1.slider');
 const spnText = document.querySelector('span.slider');
@@ -28,26 +30,35 @@ let activeText = 0;
 const timeSlide = 5000;
 let indexTxt = 0;
 const timeLetter = 20;
+/**** Write on Img Header *****/
 const addLetter = () => {
-    spnText.textContent += arrayTxtSpan[activeText][activeLetter];
     activeLetter++;
-    if (activeLetter === arrayTxtSpan[activeText].length) {
-        if (changeSlide) {
-            activeText++
-        };
-        if (activeText === arrayTxtSpan.length) {
+    spnText.textContent += slideListHeader[activeText].arrayTxtSpan[activeLetter - 1];
+
+    if (activeLetter === slideListHeader[activeText].arrayTxtSpan.length) {
+        clearInterval(indexInterval)
+
+        activeText++;
+        if (activeText === 3) {
             activeText = 0;
         }
         return setTimeout(() => {
             activeLetter = 0;
             spnText.textContent = '';
             addLetter();
-        }, timeSlide - (arrayTxtSpan[activeText].length * timeLetter) - 74);
+            active++;
 
-    }
+            if (active === slideListHeader.length) {
+                active = 0;
+            }
+            image.src = slideListHeader[active].img;
+            h1.textContent = slideListHeader[active].textH1;
+        }, timeSlide)
+    };
     setTimeout(addLetter, timeLetter)
 }
-setTimeout(addLetter(), timeSlide);
+addLetter();
+/***** Slider ******/
 const changeSlide = () => {
     active++;
     if (active === slideListHeader.length) {
@@ -55,13 +66,11 @@ const changeSlide = () => {
     }
     image.src = slideListHeader[active].img;
     h1.textContent = slideListHeader[active].textH1;
-    spnText.textContent = ''
+
+    spnText.textContent = '';
 }
-setInterval(changeSlide, timeSlide);
 
-
-
-
+let indexInterval = setInterval(changeSlide, timeSlide);
 
 navBtns.forEach(navBtn => {
     const noRefresh = (e) => {
